642944b758059249ab8efb84abfe05020edace18
helen tan <helentanmz0909@gmail.com>
2022-09-12 17:48:23 +0800

    Added UsersList page/component

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
+  // dummy data
+  const USERS = [{
+    id: 1,
+    name: 'Max Schwarz',
+    image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+    places: 3
+  }];
+
   return (
-    <h2>Users</h2>
+    <UsersList items={USERS}/>
   )
 }
 
