6bfcb3bab223ffb7f485db0a15bc0327e09082ac
helen tan <helentanmz0909@gmail.com>
2022-09-14 11:15:36 +0800

    Added route for users' places page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Switch>
           <Route path="/" exact><Users /></Route>
           <Route path="/places/new" exact><NewPlace /></Route>
-
+          <Route path="/:userId/places" exact><UserPlaces></UserPlaces></Route>
           <Redirect to="/" />
         </Switch>
       </main>
