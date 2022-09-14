05ed41acaff3ef7f32cc4acff77f503821356afb
helen tan <helentanmz0909@gmail.com>
2022-09-14 11:19:06 +0800

    minor fix

--- frontend/src/shared/components/Navigation/NavLinks.jsx
+++ frontend/src/shared/components/Navigation/NavLinks.jsx
@@ -9,7 +9,7 @@ const NavLinks = (props) => {
         <NavLink to='/' exact>All Users</NavLink>
       </li>
       <li>
-        <NavLink to='/u1/places'>My Places</NavLink>
+        <NavLink to='/1/places'>My Places</NavLink>
       </li>
       <li>
         <NavLink to='/places/new'>Add Place</NavLink>
