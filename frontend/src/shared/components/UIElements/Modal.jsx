b6ca1b78fb71eae098853442294fd198967b4254
helen tan <helentanmz0909@gmail.com>
2022-09-16 12:19:44 +0800

    Use Modal in PlaceItem

--- frontend/src/shared/components/UIElements/Modal.jsx
+++ frontend/src/shared/components/UIElements/Modal.jsx
@@ -10,18 +10,18 @@ const ModalOverlay = props => {
     <div className={`modal ${props.className}`} style={props.style}>
       <header className={`modal__header ${props.headerClass}`}>
         <h2>{props.header}</h2>
-
-        <form onSubmit = {props.onSubmit ? props.onSubmit : (event) => event.preventDefault()}>
-          {/* User can inject the contents/inputs of a form */}
-          <div className={`modal__content ${props.contentClass}`}>
-            {props.children}
-          </div>
-          {/* User can inject the submit buttons of a form */}
-          <footer className={`modal__footer ${props.footerClass}`}>
-            {props.footer}
-          </footer>
-        </form>
       </header>
+
+      <form onSubmit = {props.onSubmit ? props.onSubmit : (event) => event.preventDefault()}>
+        {/* User can inject the contents/inputs of a form */}
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        {/* User can inject the submit buttons of a form */}
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
     </div>
   )
 
