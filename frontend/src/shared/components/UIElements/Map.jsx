f41ead0bb2374fb9df9743e4d811538248705043
helen tan <helentanmz0909@gmail.com>
2022-09-17 23:57:16 +0800

    installed dotenv

--- frontend/src/shared/components/UIElements/Map.jsx
+++ frontend/src/shared/components/UIElements/Map.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import "./Map.css"
 
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+
 const Map = (props) => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
   return (
-    <div className={`map ${props.className}`} style={props.style}>
+    <div ref={mapContainer} className={`map ${props.className}`} style={props.style}>
       Map
     </div>
   )
