fe183612ac9eaec6fbecef86e4767e99f98b1dee
helen tan <helentanmz0909@gmail.com>
2022-09-18 01:01:43 +0800

    Added map marker & navigation control

--- frontend/src/shared/components/UIElements/Map.jsx
+++ frontend/src/shared/components/UIElements/Map.jsx
@@ -6,22 +6,33 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
 const Map = (props) => {
   const mapContainer = useRef(null);
-  const map = useRef(null);
+  //const map = useRef(null);
   //const [lng, setLng] = useState(-70.9);
   //const [lat, setLat] = useState(42.35);
   //const [zoom, setZoom] = useState(9);
 
   const { lat, lng, zoom } = props;
 
+  // Initialize map when componennt mounts
   useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
+    //if (map.current) return; // initialize map only once
+    const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom
     });
+
+    // Create marker
+    new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map)
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    // Clean up on unmount
+    return () => map.remove();
   }, [lat, lng, zoom]);
+
 
   return (
     <div
