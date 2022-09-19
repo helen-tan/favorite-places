2275a80664bb955074924c0056fbd87d48a07b81
helen tan <helentanmz0909@gmail.com>
2022-09-19 23:20:57 +0800

    NewPlace styles

--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Input from '../../shared/components/FormElements/Input'
+import "./NewPlace.css"
 
 const NewPlace = () => {
   return (
     <form className='place-form'>
-      <Input />
+      <Input element='input' type='text' label="Title"/>
     </form>
   )
 }
