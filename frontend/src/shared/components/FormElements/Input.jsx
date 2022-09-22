df59258d40f88bd6f8e80ef17e46288f1f4eb35d
helen tan <helentanmz0909@gmail.com>
2022-09-22 22:54:36 +0800

    Add input validator

--- frontend/src/shared/components/FormElements/Input.jsx
+++ frontend/src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react'
+import { validate } from '../../util/validators'
 import './Input.css'
 
 const inputReducer = (state, action) => {
@@ -7,7 +8,7 @@ const inputReducer = (state, action) => {
       return {
         ...state,
         value: action.val,
-        isValid: true
+        isValid: validate(action.val, action.validators)
       };
     default:
       return state;
@@ -21,7 +22,8 @@ const Input = (props) => {
   const changeHandler = e => {
     dispatch({
       type: 'CHANGE',
-      val: e.target.value
+      val: e.target.value,
+      validators: props.validators
     });
   }
 
