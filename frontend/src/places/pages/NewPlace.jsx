bc62d8e044c6fdaf84a3389933c3cb87acae93a7
helen tan <helentanmz0909@gmail.com>
2022-09-23 23:52:44 +0800

    Added description input

--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,6 +1,6 @@
 import React, {useCallback} from 'react'
 import Input from '../../shared/components/FormElements/Input'
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators'
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'
 import "./NewPlace.css"
 
 const NewPlace = () => {
@@ -8,16 +8,29 @@ const NewPlace = () => {
 
   }, []);
 
+  const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+  }, []);
+
   return (
     <form className='place-form'>
       <Input
+        id='title'
         element='input'
         type='text'
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText='Please enter a valid title.'
         onInput={titleInputHandler}
-    />
+      />
+      <Input
+        id='description'
+        element='textarea'
+        label="Description"
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+        errorText='Please enter a valid description of at least 5 characters.'
+        onInput={descriptionInputHandler}
+      />
     </form>
   )
 }
